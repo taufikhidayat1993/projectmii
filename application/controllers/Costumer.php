4aa191f6946ae430383f080f45d826be4e7b0b50
Taufiq Hidayatullah <taufikkragan@gmail.com>
2018-12-06 13:54:22 +0700

    update costumer

--- application/controllers/Costumer.php
+++ application/controllers/Costumer.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Costumer extends CI_Controller {
+class Costumer1 extends CI_Controller {
     Public function __construct() { 
         parent::__construct(); 
         $this->load->model('costumermodel','', TRUE);
